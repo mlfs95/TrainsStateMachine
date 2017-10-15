495480ecd12a603586c0ab4f7c10e8601568d172
Lucas Debatin <lucasdebatinb@gmail.com>
2017-10-15 09:19:18 -0200

    controlador de semaforos

---
+++ src/controller/TrafficManager.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import model.TrafficLight;
+import view.PNRailroad;
+
+public class TrafficManager implements model.Observer {
+	
+	 private static TrafficManager instance = null;
+	 private TrafficLight trafficLeft,trafficRight;
+	
+	   
+	    public TrafficManager(){
+	       
+	        trafficLeft = new TrafficLight();
+	        trafficRight = new TrafficLight();
+	        trafficRight.setIsGreen(true);
+	        trafficLeft.setIsGreen(false);
+	       
+	    }
+	   
+	    public static TrafficManager getInstance(){
+	        if(instance == null){
+	            instance = new TrafficManager();
+	        }
+	       
+	        return instance;
+	    }
+	    
+	    public TrafficLight getTrafficLeft() {
+			return trafficLeft;
+		}
+		
+		public TrafficLight getTrafficRight() {
+			return trafficRight;
+		}
+
+		@Override
+		public void update(Observable o, Object arg) {
+			
+		}
+
+		@Override
+		public void update() {
+			System.out.println("Observer ativado");
+			//System.out.println("quantidade de trens: " + TrainManager.getInstance().getqtdtrainsLeft() );
+			
+			if(trafficLeft.getIsGreen() == true && trafficRight.getIsGreen() == false){
+				trafficLeft.setIsGreen(false);
+				trafficRight.setIsGreen(true);
+			}
+			else if(trafficRight.getIsGreen() == true && trafficLeft.getIsGreen() == false){
+				trafficRight.setIsGreen(false);
+				trafficLeft.setIsGreen(true);
+			}
+			else if(trafficLeft.getIsGreen() == false && trafficRight.getIsGreen() == false){
+				trafficLeft.setIsGreen(true);
+			}
+			
+		}
+
+		
+
+}
